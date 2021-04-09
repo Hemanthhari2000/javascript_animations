4faba9dc5b9462863a584f476b49500ebdc9baef
Hemanth <hemanthhari2000@gmail.com>
2021-04-09 21:18:22 +0530

    page-visibility project added

---
+++ page_visibility/app.js
@@ -0,0 +1,21 @@
+let hiden = document.getElementById("hidden");
+let visble = document.getElementById("visible");
+
+document.addEventListener('visibilitychange', function(){
+    document.title = document.visibilityState;
+    var result = document.visibilityState;
+    console.log(result);
+    
+    if (result == "hidden"){
+        visble.innerHTML="";
+        var nodeHidden = document.createElement('h1');
+        nodeHidden.innerHTML = result;
+        hiden.appendChild(nodeHidden);
+    }
+    else{
+        hiden.innerHTML="";
+        var nodeVisible = document.createElement('h1');
+        nodeVisible.innerHTML = result;
+        visble.appendChild(nodeVisible);
+    }
+});
